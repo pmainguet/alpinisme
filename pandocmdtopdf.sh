03a7b131ed0d2d870a5dffb4c47218f868a8353d
Pierre Mainguet <pierre.mainguet@kelkoogroup.com>
2019-07-02 10:20:30 +0200

    small change in sh file

--- pandocmdtopdf.sh
+++ pandocmdtopdf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find * -iname "*.md" -type f -exec sh -c 'pandoc "${0}" -V linkcolor:blue -V geometry:a4paper -V geometry:margin=2cm -V mainfont="DejaVu Serif" -V monofont="DejaVu Sans Mono" -o "${0%.md}.pdf"' {} \;
+find * -iname "*.md" -type f -exec sh -c 'pandoc -f markdown-implicit_figures "${0}" -V linkcolor:blue -V geometry:a4paper -V geometry:margin=2cm -V mainfont="DejaVu Serif" -V monofont="DejaVu Sans Mono" -o "${0%.md}.pdf"' {} \;
 rm ./pdf/*.pdf
 find . -iname "*.pdf" -type f -exec mv {} ./pdf \;
